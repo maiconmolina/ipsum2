ff25e44dae9ad7bf5c9ebb657cd7fcb428499ae9
Usuario <Usuario@192.168.1.2>
2014-10-04 23:02:57 +0000

    Criadas 2 telas, e criada uma classe InterfaceUtils, que deverá ter seu método invocado no construtor das JInternalFrame

---
+++ src/view/InterfaceUtils.java
@@ -0,0 +1,10 @@
+package view;
+
+import javax.swing.JInternalFrame;
+
+public class InterfaceUtils {
+    public static void preparaTela(JInternalFrame jf){
+        jf.setClosable(true);
+        Start.addFrame(jf);
+    }
+}
