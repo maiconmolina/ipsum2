68d1cb3e92ecc3be5b55c0650e0de9b208130607
lucasstranieri <lucas_stranieri93@hotmail.com>
2014-10-15 18:12:25 -0300

    Tela de NFE

--- src/Util/Constante.java
+++ src/Util/Constante.java
@@ -13,5 +13,7 @@ public class Constante {
     public static final String razao = "Ipsum";
     public static final UfEnum uf = UfEnum.PR;
     public static final String cnpj = "13.528.093/0001-73";
-    public static final String inscricao = "210.60339-54";
+    public static final String endereco = "Rua Luiz Fabeni, n.730";
+    public static final String municipio = "Maringa";
+    public static final String email = "ipsumcostura@gmail.com";
 }
